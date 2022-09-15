3eb6e718a805cd892074fea7627284df9475a0be
Cody Seibert <cseibert113@gmail.com>
2022-09-14 22:15:25 -0400

    adding the chatting page and connecting two users

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -3,10 +3,12 @@ import { createRouter } from "./context";
 import superjson from "superjson";
 
 import { usersRouter } from "./users";
+import { matchesRouter } from "./matches";
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge("users.", usersRouter);
+  .merge("users.", usersRouter)
+  .merge("matches.", matchesRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
